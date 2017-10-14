8cf9bdfba1759fc812c7f30ce89b50a640c219cc
Pyae Hlian Moe <pyae.phm@gmail.com>
2017-10-15 03:00:53 +0800

    Add files via upload

--- codes/main.py
+++ codes/main.py
@@ -1,55 +1,91 @@
 #!/usr/bin/env python3
 
-#import sys
 import time
 import telepot
-#import json
+import re
+import csv
+import urllib.request
+import base64
+#import xlsxwriter
+import os
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
+upload = False
+regex_check = False
+
 welcome_message = "Welcome to iShareBot\nPlease select an option from keyboard"
 error_message = "You have enter an invalid option."
-weclome_keyboardLayout = [[KeyboardButton(text='/Upload'), KeyboardButton(text='/Download')]]
-upload_keyboardLayout = [[KeyboardButton(text=""), KeyboardButton(text="")]]
+hash_tag_message = "Please enter a hash_tag: (e.g. #cz1001#lec)\nPlease take note that maximum two hash_tags."
 
-def main_handle(msg):
+weclome_keyboardLayout = [
+    [KeyboardButton(text='Upload'), KeyboardButton(text='Download')]
+]
+welcome_keyboard_markup = ReplyKeyboardMarkup(keyboard=weclome_keyboardLayout,resize_keyboard=True,one_time_keyboard=True)
+
+def mainHandler(msg):
+    global upload, download, regex_check, hashTag
     content_type, chat_type, chat_id = telepot.glance(msg)
-    welcome_keyboard_markup = ReplyKeyboardMarkup(keyboard=weclome_keyboardLayout,resize_keyboard=True,one_time_keyboard=True)
     
-    #if content_type == 'text' and msg['text'] != 'Upload':
-        #bot.sendMessage(chat_id,welcome_message, reply_markup=welcome_keyboard_markup)
-    #elif content_type == 'text' and msg['text'] == 'Upload':
-        #upload_handle(msg)
-    #else:
-        #bot.sendMessage(chat_id, error_message)
     if (content_type == 'text'):
-        if (msg['text'] == '/Upload'):
-            bot.sendMessage(chat_id, "Please enter a module code")
-            bot.sendMessage(chat_id, "Please enter a ")
-            upload_handle()
-        elif (msg['text'] == '/Download'):
-            download_handle(msg)
+        if (msg['text'] == 'Upload'):
+            bot.sendMessage(chat_id, hash_tag_message)
+            upload = True
+        elif (msg['text'] == 'Download'):
+            bot.sendMessage(chat_id, hash_tag_message)
+            download = True
+        elif (re.match("^(#[A-Za-z]{2}\d{4})#(lab|lec|tut)$", msg['text'])):
+            if (upload == True):
+                hashTag = msg['text']
+                bot.sendMessage(chat_id, "Please upload a screenshot!")
+                regex_check = True
+            elif (download == True):
+                downloadHandler(msg)
+            else:
+                bot.sendMessage(chat_id, welcome_message, reply_markup=welcome_keyboard_markup)
         else:
             bot.sendMessage(chat_id,welcome_message, reply_markup=welcome_keyboard_markup)
+    elif (content_type == 'photo'):
+        if (regex_check == True):
+            if (upload == True):
+                uploadHandler(msg, hashTag)
+                #upload = False
+                #regex_check = False
+            else:
+                bot.sendMessage(chat_id, "Please upload a screenshot!!")
+        else:
+            bot.sendMessage(chat_id, welcome_message)
+            #upload = False
+            #download = False
+        
+        upload = False
+        download = False
+        regex_check = False
+    else:
+        bot.sendMessage(chat_id, error_message)
 
-def upload_handle():
-    #content_type, chat_type, chat_id = telepot.glance(msg)
-    
-    #bot.sendMessage(chat_id, "glad to enter")
-    
-    ## TODO - handle for the upload
-    ## Need 3 arguments. Module code, category and datetime
-    
-    response = bot.getUpdates()
-    pprint(response)
+def uploadHandler(msg, hashTag):
+    file_id = msg['photo'][0]['file_id']
+    file_path = msg['photo'][0]['file_path']
+    download_path = "https://api.telegram.org/file/bot473082600:AAHyecek_jYWVsVhpyWY7EIs06VtA3dP2tQ/" + file_path
+    os.chdir('../Files/')
+    bot.download_file(file_id, hashTag + ".jpg")
 
-def download_handle():
-    ## TODO - handle for the download.
-    ## 
+def downloadHandler(msg):
     print ("Download handle")
-    
+
+
+def save_file(description):
+    print("Save the file")
+
+
+
+
+
+
+
 bot = telepot.Bot('473082600:AAHyecek_jYWVsVhpyWY7EIs06VtA3dP2tQ')
-MessageLoop(bot, main_handle).run_as_thread()
+MessageLoop(bot, mainHandler).run_as_thread()
 
 # Keep the program running.
 while 1:
